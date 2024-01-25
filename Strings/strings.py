47322402dc7f6c0ef2b829c7b9f7fe63daa38bcc
Xander Gergye <xanderrgergye@gmail.com>
2024-01-25 09:02:21 -0500

    Strings Relocated

---
+++ Strings/strings.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+ 
+#Programmer: Gabe Westra
+#Program: Strings
+ 
+print("Michigan Wolverines are National Champions\n")
+ 
+print("7 + 8 =",7+8)
+print("")
+print("9 + 3 = "+ str(9+3),"\n")
+ 
+print("""\n\n\n\nHappy New Year!
+everyone, Yeah!!!""") #""" will print on multiple lines"""
