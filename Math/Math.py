bf47512c2bc71fbb759c4de1210a70ffc563d64d
Xander Gergye <xanderrgergye@gmail.com>
2024-01-25 09:04:04 -0500

    Math Relocated

---
+++ Math/Math.py
@@ -0,0 +1,12 @@
+#Programmer: X Gergye
+#Program: Math
+#Date: 1.10.2024
+
+print(9 * 9) #Multiplication
+print(9 + 9) #Addition
+print(9 - 6) #Subtraction
+print(9 / 3) #Division
+print(50 ** 3) #Exponent
+print(50 % 6) #Modulas - Takes what is leftover
+print(50 / 6) #Division with remainder - Float
+print(50 // 6) #No remainder w/ Whole number
